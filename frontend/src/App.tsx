882193a8e6c615c281c117e19e36455ad608ad31
Rawbirthh <rawbi@DESKTOP-L63L4Q7>
2025-05-11 20:12:46 +0800

    set up, installation

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <>Robert Typescript</>
+    </>
+  )
+}
+
+export default App
